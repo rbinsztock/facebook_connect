5f9be81e027feda1ae7963a1f7691a74c4410f45
Binsztock Remy <remy@rbweb.fr>
2013-03-06 02:09:55 +0100

    tadam problem solved

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  p ENV['FACEBOOK_KEY'] 
-  p ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :display => "popup"
 end
