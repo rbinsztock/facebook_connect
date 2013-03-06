ebe11a06718dab9a41d8828e7c8c668b61c4430c
Binsztock Remy <remy@rbweb.fr>
2013-03-06 01:06:26 +0100

    facebook connect via omni auth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  p ENV['FACEBOOK_KEY'] 
+  p ENV['FACEBOOK_SECRET']
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookConnect::Application.config.secret_token = 'cc6556b614b3af3ecff479109d4e20a4a6f499b66159dae8971c08516b3d1c87cd75539a832c382f53ed928119863b286bb54abde1af531d98307aea148f0aa2'
